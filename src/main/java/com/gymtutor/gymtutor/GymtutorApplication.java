089f7d3c3273b35787d5acafc73c573205068c35
Leandro Canha <leandrocanha10@gmail.com>
2025-03-24 08:41:58 -0300

    Initial commit

---
+++ src/main/java/com/gymtutor/gymtutor/GymtutorApplication.java
@@ -0,0 +1,13 @@
+package com.gymtutor.gymtutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymtutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymtutorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gymtutor/gymtutor/GymtutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gymtutor.gymtutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymtutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
