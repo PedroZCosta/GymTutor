7ce38186083d3eca1fadf0a8bbbc2dfc1a3c6bdf
Leandro Canha <leandrocanha10@gmail.com>
2025-03-31 11:12:48 -0300

    criação das tabelas image e video que estao nos ActivitiesImagesModel

--- src/main/java/com/gymtutor/gymtutor/activitiesimages/ActivitiesImagesModel.java
+++ src/main/java/com/gymtutor/gymtutor/activitiesimages/ActivitiesImagesModel.java
@@ -1,4 +1,67 @@
 package com.gymtutor.gymtutor.activitiesimages;
 
+
+import com.gymtutor.gymtutor.activities.ActivitiesModel;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(
+        name ="tb_activitiesimages"
+)
 public class ActivitiesImagesModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int imageId;
+
+    @NotBlank(message = "Nome da imagem não pode estar vazio!")
+    @Size(min = 3, max = 100, message = "Nome da imagem deve ter entre 3 e 100 caracteres.")
+    private String imageName;
+
+    @ManyToOne
+    @JoinColumn(name = "activity_id",  referencedColumnName = "activitiesId")
+    private ActivitiesModel activity;
+
+    @NotBlank(message = "Caminho da imagem não pode estar vazio!")
+    @Column(name = "image_path")
+    private String imagePath;
+
+
+    public ActivitiesImagesModel() {
+    }
+
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+    public ActivitiesModel getActivity() {
+        return activity;
+    }
+
+    public void setActivity(ActivitiesModel activity) {
+        this.activity = activity;
+    }
+
 }
--- src/main/java/com/gymtutor/gymtutor/activitiesvideos/ActivitiesVideosModel.java
+++ src/main/java/com/gymtutor/gymtutor/activitiesvideos/ActivitiesVideosModel.java
@@ -1,4 +1,67 @@
 package com.gymtutor.gymtutor.activitiesvideos;
 
+
+import com.gymtutor.gymtutor.activities.ActivitiesModel;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_activitiesvideo")
 public class ActivitiesVideosModel {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private int videoId;
+
+    @NotBlank(message = "O nome do video nao pode esta vazio")
+    @Size(min = 3, max = 100, message = "O nome do vídeo deve ter entre 3 e 100 caracteres.")
+    private String videoName;
+
+    @NotBlank(message = "O link do vídeo não pode estar vazio!")
+    @Column(name = "video_link", length = 500)
+    private String videoLink;
+
+    @NotNull(message = "O vídeo deve estar associado a um exercício!")
+    @ManyToOne
+    @JoinColumn(name = "activity_id", referencedColumnName = "activitiesId")
+    private ActivitiesModel activity;
+
+    public ActivitiesVideosModel() {
+    }
+
+
+    public int getVideoId() {
+        return videoId;
+    }
+
+    public void setVideoId(int videoId) {
+        this.videoId = videoId;
+    }
+
+    public String getVideoName() {
+        return videoName;
+    }
+
+    public void setVideoName(String videoName) {
+        this.videoName = videoName;
+    }
+
+    public String getVideoLink() {
+        return videoLink;
+    }
+
+    public void setVideoLink(String videoLink) {
+        this.videoLink = videoLink;
+    }
+
+    public ActivitiesModel getActivity() {
+        return activity;
+    }
+
+    public void setActivity(ActivitiesModel activity) {
+        this.activity = activity;
+    }
+
 }
