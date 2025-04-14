bd32ff34e658a44e29f79a1b3b1c3685497c82ab
FabioVisentainer <fabio_visentainer@hotmail.com>
2025-04-14 16:43:13 -0300

    refactor(config): isolar propriedade de upload para imagens de atividades
    refactor(config): isolate upload property for activity images
    
    - Alterado UploadProperties para utilizar `${upload.dir.activities}`
    - Método `getUploadDir()` renomeado para `getUploadDirActivities()`
    - Preparação para suportar múltiplos diretórios de upload (usuários, etc)

--- src/main/java/com/gymtutor/gymtutor/config/UploadProperties.java
+++ src/main/java/com/gymtutor/gymtutor/config/UploadProperties.java
@@ -6,10 +6,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class UploadProperties {
 
-    @Value("${upload.dir}")
-    private String uploadDir;
+    @Value("${upload.dir.activities}")
+    private String uploadDirActivities;
 
-    public String getUploadDir() {
-        return uploadDir;
+    public String getUploadDirActivities() {
+        return uploadDirActivities;
     }
 }
