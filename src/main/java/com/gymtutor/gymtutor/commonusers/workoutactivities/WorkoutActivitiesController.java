be5d4c926c9c89145242ca0cfcd3b5c0d0bdeed8
FabioVisentainer <fabio_visentainer@hotmail.com>
2025-04-25 23:21:53 -0300

    refactor(workout-activities): centralizada a lógica de carregamento de visualização
    refactor(workout-activities): centralized view loading logic
    
    - Extraída a lógica de carregamento dos dados da tela de vinculação de atividades para o método `loadView`
    - Atualizado o método `handleRequest` para receber o `workoutId` e reutilizar a lógica de carregamento também em erros
    - Reduzida duplicação de código e melhorada a legibilidade geral do controller

--- src/main/java/com/gymtutor/gymtutor/commonusers/workoutactivities/WorkoutActivitiesController.java
+++ src/main/java/com/gymtutor/gymtutor/commonusers/workoutactivities/WorkoutActivitiesController.java
@@ -42,25 +42,8 @@ public class WorkoutActivitiesController {
             Model model,
             @AuthenticationPrincipal CustomUserDetails loggedUser,
             RedirectAttributes redirectAttributes) {
-        return handleRequest(redirectAttributes, model, null, null, () -> {
-            WorkoutModel workout = workoutService.findById(workoutId);
-            List<ActivitiesModel> allActivities = activitiesRepository.findAll();
-
-            List<Integer> linkedActivitiesIds = workout.getWorkoutActivities().stream()
-                    .map(link -> link.getActivity().getActivitiesId())
-                    .collect(Collectors.toList());
-
-            List<WorkoutActivitiesModel> linkedActivities = workout.getWorkoutActivities()
-                    .stream()
-                    .sorted(Comparator.comparingInt(link -> link.getSequence()))
-                    .collect(Collectors.toList());
-
-            model.addAttribute("workout", workout);
-            model.addAttribute("activities", allActivities);
-            model.addAttribute("linkedActivitiesIds", linkedActivitiesIds);
-            model.addAttribute("linkedActivities", linkedActivities);
-            model.addAttribute("body", "student/workout/linkactivities");
-
+        return handleRequest(redirectAttributes, model, null, null, workoutId, () -> {
+            loadView(workoutId, model);
             return "/fragments/layout";
         });
     }
@@ -81,7 +64,7 @@ public class WorkoutActivitiesController {
             return "redirect:/student/workout/" + workoutId + "/linkactivities";
         }
 
-        return handleRequest(redirectAttributes, model, "/student/workout/" + workoutId + "/linkactivities", null, () -> {
+        return handleRequest(redirectAttributes, model, "/student/workout/linkactivities", null, workoutId, () -> {
             WorkoutModel workout = workoutService.findById(workoutId);
             ActivitiesModel activity = activitiesRepository.findById(formDTO.getActivitiesId()).orElseThrow();
             workoutActivitiesService.linkWorkoutActivityToWorkout(workout, activity, formDTO.getReps());
@@ -97,7 +80,7 @@ public class WorkoutActivitiesController {
             RedirectAttributes redirectAttributes,
             Model model
     ) {
-        return handleRequest(redirectAttributes, model, "/student/workout/" + workoutId + "/linkactivities", null, () -> {
+        return handleRequest(redirectAttributes, model, "/student/workout/linkactivities", null, workoutId, () -> {
             WorkoutModel workout = workoutService.findById(workoutId);
             ActivitiesModel activity = activitiesRepository.findById(formDTO.getActivitiesId()).orElseThrow();
             workoutActivitiesService.unlinkWorkoutActivityFromWorkout(workout, activity);
@@ -132,11 +115,11 @@ public class WorkoutActivitiesController {
         return "/fragments/layout";
     }
 
-    private String handleRequest(RedirectAttributes redirectAttributes, Model model, String view, WorkoutActivitiesModel workoutActivitiesModel, WorkoutActivitiesController.RequestHandler block){
+    private String handleRequest(RedirectAttributes redirectAttributes, Model model, String view, WorkoutActivitiesModel workoutActivitiesModel, int workoutId, WorkoutActivitiesController.RequestHandler block){
         try{
             return block.execute();
         }catch(Exception ex){
-            return handleException(ex, model, workoutActivitiesModel, view, redirectAttributes);
+            return handleException(ex, model, workoutActivitiesModel, view, redirectAttributes, workoutId);
         }
     }
 
@@ -145,20 +128,20 @@ public class WorkoutActivitiesController {
         String execute();
     }
 
-    public String handleException(Exception ex, Model model, WorkoutActivitiesModel workoutActivitiesModel, String view, RedirectAttributes redirectAttributes){
+    public String handleException(Exception ex, Model model, WorkoutActivitiesModel workoutActivitiesModel, String view, RedirectAttributes redirectAttributes, int workoutId){
         return switch (ex){
             case IllegalArgumentException illegalArgumentException ->
-                    handleIllegalArgumentException(illegalArgumentException, model, workoutActivitiesModel, view);
+                    handleIllegalArgumentException(illegalArgumentException, model, workoutActivitiesModel, view, workoutId);
             case IllegalAccessException illegalAccessException ->
                     handleIllegalAccessException(illegalAccessException, redirectAttributes);
             case DataIntegrityViolationException ignored ->
-                    handleDataIntegrityViolationException(model, workoutActivitiesModel, view);
-            case null, default -> handleUnexpectedException(model, workoutActivitiesModel, view);
+                    handleDataIntegrityViolationException(model, workoutActivitiesModel, view, workoutId);
+            case null, default -> handleUnexpectedException(model, workoutActivitiesModel, view, workoutId);
         };
     }
 
-    private String handleIllegalArgumentException(IllegalArgumentException ex, Model model, WorkoutActivitiesModel workoutActivitiesModel, String view){
-        return handleError(ex.getMessage(), model, workoutActivitiesModel, view);
+    private String handleIllegalArgumentException(IllegalArgumentException ex, Model model, WorkoutActivitiesModel workoutActivitiesModel, String view, int workoutId){
+        return handleError(ex.getMessage(), model, workoutActivitiesModel, view, workoutId);
     }
 
     private String handleIllegalAccessException(IllegalAccessException ex, RedirectAttributes redirectAttributes){
@@ -166,19 +149,20 @@ public class WorkoutActivitiesController {
         return "redirect:/student/workout";
     }
 
-    private String handleDataIntegrityViolationException(Model model, WorkoutActivitiesModel workoutActivitiesModel, String view){
-        return handleError("Erro de integridade de dados.", model, workoutActivitiesModel, view);
+    private String handleDataIntegrityViolationException(Model model, WorkoutActivitiesModel workoutActivitiesModel, String view, int workoutId){
+        return handleError("Erro de integridade de dados.", model, workoutActivitiesModel, view, workoutId);
     }
 
-    private String handleUnexpectedException(Model model, WorkoutActivitiesModel workoutActivitiesModel, String view){
-        return handleError("Erro inesperado. Tente novamente.", model, workoutActivitiesModel, view);
+    private String handleUnexpectedException(Model model, WorkoutActivitiesModel workoutActivitiesModel, String view, int workoutId){
+        return handleError("Erro inesperado. Tente novamente.", model, workoutActivitiesModel, view, workoutId);
 
     }
 
-    private String handleError(String errorMessage, Model model, WorkoutActivitiesModel workoutActivitiesModel, String view){
+    private String handleError(String errorMessage, Model model, WorkoutActivitiesModel workoutActivitiesModel, String view, int workoutId){
         if (model != null) {
             model.addAttribute("errorMessage", errorMessage);
             model.addAttribute("workoutActivitiesModel", workoutActivitiesModel);
+            loadView(workoutId , model);
         }
         if(view != null){
             assert model != null;
@@ -187,6 +171,26 @@ public class WorkoutActivitiesController {
         }else{
             return "redirect:/student/workout";
         }
+    }
+
+    private void loadView(int workoutId, Model model){
+        WorkoutModel workout = workoutService.findById(workoutId);
+        List<ActivitiesModel> allActivities = activitiesRepository.findAll();
+
+        List<Integer> linkedActivitiesIds = workout.getWorkoutActivities().stream()
+                .map(link -> link.getActivity().getActivitiesId())
+                .collect(Collectors.toList());
+
+        List<WorkoutActivitiesModel> linkedActivities = workout.getWorkoutActivities()
+                .stream()
+                .sorted(Comparator.comparingInt(link -> link.getSequence()))
+                .collect(Collectors.toList());
+
+
+        model.addAttribute("workout", workout);
+        model.addAttribute("activities", allActivities);
+        model.addAttribute("linkedActivitiesIds", linkedActivitiesIds);
+        model.addAttribute("linkedActivities", linkedActivities);
     }
 
 
