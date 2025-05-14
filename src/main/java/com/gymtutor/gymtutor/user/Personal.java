dc32c4f1fdfe629f692d05878f9593a45d0b9255
FabioVisentainer <fabio_visentainer@hotmail.com>
2025-05-14 20:29:57 -0300

    feat(personal): adicionados campos de estado, aprovação e rejeição ao modelo
    feat(personal): added state, approval, and rejection fields to personal model
    
    - Corrigido nome do campo de `personalCREEF` para `personalCREF`
    - Adicionado campo `state` com enumeração para representar o estado do registro profissional
    - Inseridos campos `isApproved`, `isRejected` e `rejectReason` para controle do fluxo de aprovação do personal
    - Aplicadas validações com `@NotNull` e `@Size` para garantir integridade dos dados

--- src/main/java/com/gymtutor/gymtutor/user/Personal.java
+++ src/main/java/com/gymtutor/gymtutor/user/Personal.java
@@ -2,6 +2,9 @@ package com.gymtutor.gymtutor.user;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 
 @Entity
 @Table(name = "tb_personal")
@@ -11,12 +14,23 @@ public class Personal {
     private int personalId;
 
     @NotBlank
-    private String personalCREEF;
+    private String personalCREF;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private State state;
 
     @OneToOne
     @MapsId
     @JoinColumn(name = "personalId") // mapeia o mesmo id do User
     private User user;
+
+    private boolean isApproved = false;
+
+    private boolean isRejected = false;
+
+    @Size(min = 5, max = 50, message = "O motivo da recusa deve ter entre 5 e 50 caracteres")
+    private String rejectReason;
 
     // Getters and Setters
     public int getPersonalId() {
@@ -28,11 +42,11 @@ public class Personal {
     }
 
     public String getPersonalCREEF() {
-        return personalCREEF;
+        return personalCREF;
     }
 
     public void setPersonalCREEF(String personalCREEF) {
-        this.personalCREEF = personalCREEF;
+        this.personalCREF = personalCREEF;
     }
 
     public User getUser() {
@@ -42,4 +56,25 @@ public class Personal {
     public void setUser(User user) {
         this.user = user;
     }
+
+    public String getPersonalCREF() { return personalCREF; }
+
+    public void setPersonalCREF(String personalCREF) { this.personalCREF = personalCREF; }
+
+    public State getState() { return state; }
+
+    public void setState(State state) { this.state = state; }
+
+    public boolean isApproved() {return isApproved;}
+
+    public void setApproved(boolean approved) {isApproved = approved;}
+
+    public boolean isRejected() {return isRejected;}
+
+    public void setRejected(boolean rejected) {isRejected = rejected;}
+
+    public String getRejectReason() {return rejectReason;}
+
+    public void setRejectReason(String rejectReason) {this.rejectReason = rejectReason;}
 }
+
