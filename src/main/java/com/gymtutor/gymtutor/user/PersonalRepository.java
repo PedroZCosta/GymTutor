08f5c32322a3c50dccf83ee467fe70ce35eaf9cc
FabioVisentainer <fabio_visentainer@hotmail.com>
2025-05-14 21:55:10 -0300

    feat(personal): adicionada busca por personals pendentes de análise
    feat(personal): added query for pending personals
    
    - Adicionado método `findByIsApprovedFalseAndIsRejectedFalse` ao repositório `PersonalRepository`
    - Permite recuperar todos os registros de personal que ainda não foram aprovados nem rejeitados
    - Útil para a funcionalidade de análise de CREFs por administradores

--- src/main/java/com/gymtutor/gymtutor/user/PersonalRepository.java
+++ src/main/java/com/gymtutor/gymtutor/user/PersonalRepository.java
@@ -4,10 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface PersonalRepository extends JpaRepository<Personal, Integer> {
 
     Optional<Personal> findByUser(User user);
+
+    List<Personal> findByIsApprovedFalseAndIsRejectedFalse();
 
 }
