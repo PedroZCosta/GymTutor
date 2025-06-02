d42d3c177a575e249c1467344297301ecf1eaaa8
Pedro Costa <161760181+PedroZCosta@users.noreply.github.com>
2025-06-02 10:25:54 -0300

    fix(workoutPlan): ajustando para os PBIs

--- src/main/java/com/gymtutor/gymtutor/commonusers/workoutplan/WorkoutPlanModel.java
+++ src/main/java/com/gymtutor/gymtutor/commonusers/workoutplan/WorkoutPlanModel.java
@@ -19,7 +19,7 @@ public class WorkoutPlanModel {
     private int workoutPlanId;
 
     @NotBlank(message = "Nome não pode estar vazio!")
-    @Size(min = 2, max=50 , message = "Este campo deve ter entre 2 e 50 caracteres.")
+    @Size(min = 2, max=10 , message = "Este campo deve ter entre 2 e 10 caracteres.")
     private String workoutPlanName;
 
     @NotBlank(message = "Nome não pode estar vazio!")
